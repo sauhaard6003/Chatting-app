0fc5118e7fa333ce68bd8f2659bf402a8fb00973
sauhaard6003 <78654368+sauhaard6003@users.noreply.github.com>
2023-04-13 15:05:38 +0530

    Add files via upload

---
+++ App/server.js
@@ -0,0 +1,24 @@
+const io = require('socket.io')(3000) // create server
+
+const users = {}
+
+
+io.on('connection', socket => {
+
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+
+  //socket.emit('chat-message', 'hello world')
+  socket.on('send-chat', (data, acknowledgment) => {
+
+    socket.broadcast.emit('message-received', {data: data, user: users[socket.id]})
+    acknowledgment();
+  })
+
+  socket.on('disconnect', ()=> {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
